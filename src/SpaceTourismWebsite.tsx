b229aef7f5857a3e830169748bf15a480bdcd067
MasterDev <juanpb.96@gmail.com>
2023-08-11 15:49:26 -0500

    Set default background color to prevent white flash when page is loading

--- src/SpaceTourismWebsite.tsx
+++ src/SpaceTourismWebsite.tsx
@@ -1,4 +1,20 @@
+import { useEffect } from 'react';
+import './styles/main.scss';
+
 export const SpaceTourismWebsite = () => {
+
+  useEffect(() => {
+    const removeStyleAttribute = () => {
+      document.body.removeAttribute('style');
+    }
+
+    window.addEventListener('load', removeStyleAttribute);
+  
+    return () => {
+      window.removeEventListener('load', removeStyleAttribute);
+    }
+  }, []);
+
   return (
     <div>SpaceTourism</div>
   )
--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,6 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { SpaceTourismWebsite } from './SpaceTourismWebsite'
-
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { SpaceTourismWebsite } from './SpaceTourismWebsite';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
