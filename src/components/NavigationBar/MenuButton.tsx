3a31d9fe078ce47f4065799b3b09760a4c56bc5b
MasterDev <juanpb.96@gmail.com>
2023-08-27 15:19:34 -0500

    Include variants and reduced motion support for middle rect

--- src/components/NavigationBar/MenuButton.tsx
+++ src/components/NavigationBar/MenuButton.tsx
@@ -1,9 +1,8 @@
-import { SVGMotionProps, motion } from 'framer-motion';
-import { RefAttributes } from 'react';
-import { JSX } from 'react/jsx-runtime';
+import { SVGMotionProps, motion, useReducedMotion } from 'framer-motion';
+import { bottomRectVariants, middleRectVariants, svgVariants, topRectVariants } from './animations/menu-button-variants';
 import styles from './styles/menu-button.module.scss';
 
-const Rect = (props: JSX.IntrinsicAttributes & SVGMotionProps<SVGRectElement> & RefAttributes<SVGRectElement>) => {
+const Rect = (props: SVGMotionProps<SVGRectElement>) => {
   return (
     <motion.rect
       width="24"
@@ -20,8 +19,8 @@ interface MenuButtonProps {
 }
 
 export const MenuButton = ({isOpen, onToggle}: MenuButtonProps) => {
-  // TODO: Include reduced motion
-  // TODO: Try implementing variants
+  const shouldReduceMotion = useReducedMotion();
+  const animateVariant = isOpen ? 'open' : 'closed';
 
   return (
     <button
@@ -35,34 +34,30 @@ export const MenuButton = ({isOpen, onToggle}: MenuButtonProps) => {
         height="21"
         viewBox="0 0 24 21"
         fill="none"
-        initial={{ rotate: 0 }}
-        animate={{ rotate: isOpen ? 90 : 0 }}
+        initial={false}
+        animate={animateVariant}
+        variants={svgVariants}
       >
         <Rect
           className="top"
-          initial={{ rotate: 0, y: 0 }}
-          animate={{
-            rotate: isOpen ? [0, 0, -45] : [-45, 0, 0],
-            y: isOpen ? 9 : 0
-          }}
-          transition={{ duration: 0.3, times: [0, 0.5, 1] }}
+          initial={false}
+          animate={animateVariant}
+          variants={topRectVariants}
         />
         <Rect
           className="middle"
           y="9"
-          initial={{ opacity: 1 }}
-          animate={{ opacity: isOpen ? 0 : 1 }}
-          transition={{ duration: 0.3 }}
+          custom={shouldReduceMotion}
+          initial={false}
+          animate={animateVariant}
+          variants={middleRectVariants}
         />
         <Rect
           className="bottom"
           y="18"
-          initial={{ rotate: 0, y: 0 }}
-          animate={{
-            rotate: isOpen ? [0, 0, 45] : [45, 0, 0],
-            y: isOpen ? -9 : 0
-          }}
-          transition={{ duration: 0.3, times: [0, 0.5, 1] }}
+          initial={false}
+          animate={animateVariant}
+          variants={bottomRectVariants}
         />
       </motion.svg>               
     </button>
