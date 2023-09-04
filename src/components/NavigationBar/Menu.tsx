04822d551c6da19e3575f2f55d12ad6a0adab9d8
MasterDev <juanpb.96@gmail.com>
2023-09-03 22:02:46 -0500

    Include NavLinks, and implement active selection with motion component

--- src/components/NavigationBar/Menu.tsx
+++ src/components/NavigationBar/Menu.tsx
@@ -1,3 +1,6 @@
+import { useEffect, useRef, useState } from 'react';
+import { NavLink } from 'react-router-dom';
+import { motion } from 'framer-motion';
 import styles from './styles/menu.module.scss';
 
 /* TODO:
@@ -6,15 +9,57 @@ import styles from './styles/menu.module.scss';
   - Validate behavior on viewport resize 
 */
 
+const PAGES = ['Home', 'Destination', 'Crew', 'Technology'];
+
+const getActiveClass = ({isActive}: {isActive: boolean}) => {
+  return isActive ? styles['active'] : '';
+}
+
 export const Menu = () => {
+  const [activeItemIndex, setActiveItemIndex] = useState(0);
+  const [initialY, setInitialY] = useState(0);
+  const olRef = useRef<HTMLOListElement>(null);
+
+  useEffect(() => {
+    if (olRef.current?.firstElementChild) {
+      const rect = olRef.current.firstElementChild.getBoundingClientRect();
+      setInitialY(rect.top);
+      console.log(rect.top);
+      
+    }
+  }, []);
+
+  const onLinkClick = (index: number) => {
+    setActiveItemIndex(index);
+  };
+
   return (
     <nav className={styles['menu']}>
-      <ol>
-        <li>Home</li>
-        <li>Destination</li>
-        <li>Crew</li>
-        <li>Technology</li>
+      <ol ref={olRef}>
+        {
+          PAGES.map((page, index) => (
+            <li key={page}>
+              <NavLink
+                to={`/${page}`}
+                className={getActiveClass}
+                onClick={() => onLinkClick(index)}
+              >
+                {page}
+              </NavLink>
+            </li>
+          ))
+        }
       </ol>
+      {
+        initialY > 0 && (
+          <motion.div
+            key={initialY}
+            className={styles['bar']}
+            initial={{ y: initialY }}
+            animate={{ y: activeItemIndex * 50 + initialY }}
+          />
+        )
+      }
     </nav>
   );
 };
