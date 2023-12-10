b65f50de6c03c45f212e8fb3eb58b01bfd106aac
MasterDev <juanpb.96@gmail.com>
2023-12-10 18:22:18 -0500

    Add navbar layout

--- src/components/NavigationBar/NavigationBar.tsx
+++ src/components/NavigationBar/NavigationBar.tsx
@@ -1,5 +1,19 @@
+import { useState } from 'react';
+import { MenuButton } from './MenuButton';
+import { Menu } from './Menu';
+
 export const NavigationBar = () => {
+  // TODO: Remove Menu button on tablet
+  const [isOpen, setIsOpen] = useState(false);
+  
+  const onToggle = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
-    <div>NavigationBar</div>
+    <div>
+      <MenuButton isOpen={isOpen} onToggle={onToggle} />
+      <Menu />
+    </div>
   );
 };
