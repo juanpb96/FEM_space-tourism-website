82172e2fd07407ff0039d1a170a31c9bf2533029
MasterDev <juanpb.96@gmail.com>
2024-02-01 16:01:22 -0500

    Create component structure and add styles

--- src/components/Logo/Logo.tsx
+++ src/components/Logo/Logo.tsx
@@ -1,5 +1,23 @@
+import styles from './styles/logo.module.scss';
+import { VisuallyHidden } from '../VisuallyHidden/VisuallyHidden';
+
 export const Logo = () => {
   return (
-    <div>Logo</div>
+    <div className={styles['wrapper']}>
+      <VisuallyHidden>Space Tourism Website</VisuallyHidden>
+      <svg 
+        xmlns="http://www.w3.org/2000/svg" 
+        width="48" 
+        height="48"
+        viewBox="0 0 48 48"
+        role="presentation"
+        className={styles['image']}
+      >
+        <g fill="none" fillRule="evenodd">
+          <circle cx="24" cy="24" r="24" fill="#FFF"/>
+          <path fill="#0B0D17" d="M24 0c0 16-8 24-24 24 15.718.114 23.718 8.114 24 24 0-16 8-24 24-24-16 0-24-8-24-24z"/>
+        </g>
+      </svg>
+    </div>
   );
 };
