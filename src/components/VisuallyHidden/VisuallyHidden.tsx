177fe41266554031682b28b7d87921b2be3e35c1
MasterDev <juanpb.96@gmail.com>
2024-02-03 15:19:20 -0500

    Add missing type for component with children

--- src/components/VisuallyHidden/VisuallyHidden.tsx
+++ src/components/VisuallyHidden/VisuallyHidden.tsx
@@ -1,6 +1,7 @@
+import { PropsWithChildren } from 'react';
 import styles from './styles/visually-hidden.module.scss'
 
-export const VisuallyHidden = ({children}) => {
+export const VisuallyHidden = ({children}: PropsWithChildren) => {
   return (
     <div className={styles['sr-only']}>
       {children}
