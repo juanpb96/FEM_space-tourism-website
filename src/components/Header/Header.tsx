d3802354816a0756a2abeb7681af6db985bfa5b6
MasterDev <juanpb.96@gmail.com>
2024-02-04 19:12:07 -0500

    Add Header on mobile version

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,9 +1,12 @@
 import styles from './styles/header.module.scss';
+import { Logo } from '../Logo/Logo';
+import { NavigationBar } from '../NavigationBar/NavigationBar';
 
 export const Header = () => {
   return (
     <div className={styles['wrapper']}>
-      Header
+      <Logo />
+      <NavigationBar />
     </div>
   );
 };
--- src/components/Header/stories/Header.stories.tsx
+++ src/components/Header/stories/Header.stories.tsx
@@ -1,13 +1,27 @@
 import type { Meta, StoryObj } from '@storybook/react';
 
 import { Header } from '../Header';
+import { chromaticViewport, defaultViewport } from '../../constants/stories-viewports';
+import { withRouter } from '../../helpers/stories/withRouter';
 
 const meta: Meta<typeof Header> = {
   title: 'Components/Header',
   component: Header,
+  decorators: [withRouter]
 };
 
 export default meta;
 type Story = StoryObj<typeof Header>;
+
+export const HeaderOnMobile: Story = {
+  parameters: {
+    viewport: {
+      defaultViewport: defaultViewport.mobile
+    },
+    chromatic: {
+      viewports: [chromaticViewport.mobile]
+    }
+  }
+};
 
 export const Default: Story = {};
