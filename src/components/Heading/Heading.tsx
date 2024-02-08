68bb53c3aa151bd0cf7e37636fbcf48143380a50
MasterDev <juanpb.96@gmail.com>
2024-02-08 16:33:50 -0500

    Create initial files for Heading component

---
+++ src/components/Heading/Heading.tsx
@@ -0,0 +1,9 @@
+import styles from './styles/heading.module.scss';
+
+export const Heading = () => {
+  return (
+    <div className={styles['wrapper']}>
+      Heading
+    </div>
+  );
+};
---
+++ src/components/Heading/stories/Heading.stories.tsx
@@ -0,0 +1,13 @@
+import type { Meta, StoryObj } from '@storybook/react';
+
+import { Heading } from '../Heading';
+
+const meta: Meta<typeof Heading> = {
+  title: 'Components/Heading',
+  component: Heading,
+};
+
+export default meta;
+type Story = StoryObj<typeof Heading>;
+
+export const Default: Story = {};
