44e957c0f05d20564ff445018b40f3c2d43cb663
MasterDev <juanpb.96@gmail.com>
2024-02-29 18:09:49 -0500

    Add component basic markup

--- src/components/InnerNavigationBar/InnerNavigationBar.tsx
+++ src/components/InnerNavigationBar/InnerNavigationBar.tsx
@@ -1,9 +1,17 @@
 import styles from './styles/inner-navigation-bar.module.scss';
 
+const PAGES = ['Moon', 'Mars', 'Europa', 'Titan'];
+
 export const InnerNavigationBar = () => {
   return (
-    <div className={styles['wrapper']}>
-      InnerNavigationBar
-    </div>
+    <nav className={styles['wrapper']}>
+      <ol>
+        {PAGES.map(page => (
+          <li key={page}>
+            {page}
+          </li>
+        ))}
+      </ol>
+    </nav>
   );
 };
