f93e7011363b8003b88c64eaf536831098be5552
MasterDev <juanpb.96@gmail.com>
2024-03-02 18:45:14 -0500

    Include useLocationBar

--- src/components/NavigationBar/Menu.tsx
+++ src/components/NavigationBar/Menu.tsx
@@ -1,57 +1,20 @@
-import { MouseEvent, useEffect, useRef, useState } from 'react';
-import { NavLink } from 'react-router-dom';
 import { motion } from 'framer-motion';
-import styles from './styles/menu.module.scss';
+import { MouseEvent } from 'react';
+import { NavLink } from 'react-router-dom';
 import { useScreenType } from '../../hooks/useScreenType';
 import { menuVariants } from './animations/menu.variants';
 import { getActiveClass, getBarAnimation } from './utils/Menu.utils';
+import { useLocationBar } from '../../hooks/useLocationBar';
+import styles from './styles/menu.module.scss';
 
 const PAGES = ['Home', 'Destination', 'Crew', 'Technology'];
 
-const baseFontSize = parseFloat(window.getComputedStyle(document.documentElement).getPropertyValue('font-size'));
-
 export const Menu = () => {
-  const [activeMenuOptionIndex, setActiveMenuOptionIndex] = useState(0);
-  const [options, setOptions] = useState<HTMLLIElement[]>([]);
+  const { olRef, barRef, options, activeMenuOptionIndex, setActiveMenuOptionIndex } = useLocationBar();
   const screenType = useScreenType();
-  const olRef = useRef<HTMLOListElement>(null);
-  const barRef = useRef<HTMLDivElement>(null);  
 
-  useEffect(() => {
-    const olElement = olRef.current;
-
-    if (olElement) {
-      const liElements = Array.from(olElement.getElementsByTagName('li'));
-      setOptions(liElements);
-    }
-  }, [])
-
-  useEffect(() => {
-    const barElement = barRef.current;
-
-    const resizeObserver = new ResizeObserver((entries) => {      
-      if (barElement) {
-        const activeMenuOptionWidth = Math.round(entries[0].contentBoxSize[0].inlineSize);
-        barElement.style.width = `${activeMenuOptionWidth / baseFontSize}rem`; 
-      }
-    });
-    
-    const calculateBarWidth = () => {
-      if (options.length && barElement) {    
-        const activeMenuOption = options[activeMenuOptionIndex];            
-        resizeObserver.observe(activeMenuOption);
-      }
-    }
-
-    calculateBarWidth();
-
-    return () => {
-      resizeObserver.disconnect();
-    }
-  }, [activeMenuOptionIndex, options]);
-
-  const onLinkClick = (e: MouseEvent<HTMLAnchorElement, globalThis.MouseEvent>, index: number) => {
-    e.preventDefault();   
+  const onLinkClick = (e: MouseEvent, index: number) => {
+    e.preventDefault();
     setActiveMenuOptionIndex(index);
   };
 
