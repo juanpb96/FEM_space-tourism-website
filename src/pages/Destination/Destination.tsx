aeedd87c7972463a01907287bc05bf4ac65ea9c9
MasterDev <juanpb.96@gmail.com>
2024-03-04 17:36:39 -0500

    Add page markup

--- src/pages/Destination/Destination.tsx
+++ src/pages/Destination/Destination.tsx
@@ -1,9 +1,35 @@
+import { Description } from '../../components/Description/Description';
+import { Heading } from '../../components/Heading/Heading';
+import { InnerNavigationBar } from '../../components/InnerNavigationBar/InnerNavigationBar';
+import { Subtitle } from '../../components/Subtitle/Subtitle';
 import styles from './styles/destination.module.scss';
+import { DestinationDetails } from '../../components/DestinationDetails/DestinationDetails';
 
 export const Destination = () => {
   return (
-    <div className={styles['wrapper']}>
-      Destination
-    </div>
+    <main className={styles['wrapper']}>
+      <Subtitle
+        prefix='01'
+        title='Pick your destination'
+      />
+      <img src="./assets/destination/image-moon.png" alt="" />
+      <InnerNavigationBar />
+      <Heading
+        text='Moon'
+        variant='medium'
+      />
+      <Description>
+        See our planet as you’ve never seen it before. A perfect relaxing trip away to help regain perspective and come back refreshed. While you’re there, take in some history by visiting the Luna 2 and Apollo 11 landing sites.
+      </Description>
+      <hr />
+      <DestinationDetails
+        title='Avg. Distance'
+        value='384,400 km'
+      />
+      <DestinationDetails
+        title='Est. Travel Time'
+        value='3 days'
+      />
+    </main>
   );
 };
--- src/pages/Destination/stories/Destination.stories.tsx
+++ src/pages/Destination/stories/Destination.stories.tsx
@@ -1,16 +1,30 @@
 import type { Meta, StoryObj } from '@storybook/react';
 
 import { Destination } from '../Destination';
+import { chromaticViewport, defaultViewport } from '../../../components/constants/stories-viewports';
 
 const meta: Meta<typeof Destination> = {
   title: 'Pages/Destination',
   component: Destination,
+  parameters: {
+    layout: 'fullscreen'
+  }
 };
 
 export default meta;
 type Story = StoryObj<typeof Destination>;
 
-export const DestinationOnMobile: Story = {};
+export const DestinationOnMobile: Story = {
+  // TODO: Consider moving this params block into a separate file to centralize it across stories
+  parameters: {
+    viewport: {
+      defaultViewport: defaultViewport.mobile
+    },
+    chromatic: {
+      viewports: [chromaticViewport.mobile]
+    }
+  }
+};
 
 export const DestinationOnTablet: Story = {};
 
