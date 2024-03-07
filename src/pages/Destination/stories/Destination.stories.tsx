846cfdd46fd22c67e9c1039ad9fa4c3f7d1d5057
MasterDev <juanpb.96@gmail.com>
2024-03-07 16:53:26 -0500

    Add viewport params for page in Storybook

--- src/pages/Destination/stories/Destination.stories.tsx
+++ src/pages/Destination/stories/Destination.stories.tsx
@@ -1,7 +1,7 @@
 import type { Meta, StoryObj } from '@storybook/react';
 
 import { Destination } from '../Destination';
-import { chromaticViewport, defaultViewport } from '../../../components/constants/stories-viewports';
+import { mobileParameters, tabletParameters } from '../../../components/constants/stories-viewports';
 
 const meta: Meta<typeof Destination> = {
   title: 'Pages/Destination',
@@ -15,17 +15,11 @@ export default meta;
 type Story = StoryObj<typeof Destination>;
 
 export const DestinationOnMobile: Story = {
-  // TODO: Consider moving this params block into a separate file to centralize it across stories
-  parameters: {
-    viewport: {
-      defaultViewport: defaultViewport.mobile
-    },
-    chromatic: {
-      viewports: [chromaticViewport.mobile]
-    }
-  }
+  parameters: mobileParameters
 };
 
-export const DestinationOnTablet: Story = {};
+export const DestinationOnTablet: Story = {
+  parameters: tabletParameters
+};
 
 export const Default: Story = {};
