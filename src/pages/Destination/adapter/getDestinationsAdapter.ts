c1398e2b9fe5278cbf6f47b0b7f21fbd30b96af9
MasterDev <juanpb.96@gmail.com>
2024-03-29 18:11:37 -0500

    Add getDestinationsAdapter

---
+++ src/pages/Destination/adapter/getDestinationsAdapter.ts
@@ -0,0 +1,19 @@
+import type { Destinations } from "../types";
+
+const getDestinations = async () => {
+  try {
+    // TODO: Update URL when resource is defined for prod env
+    const response = await fetch('http://localhost:3000/destinations');
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+// Inspired by Nik Sumeiko: https://www.youtube.com/live/DQwNqSEvpk8?si=ZrzqzQL2EpENSXHQ&t=4064
+export const getDestinationsAdapter = (options?: { get(): Promise<Destinations> }) => {
+  const get = options?.get ?? getDestinations;
+
+  return () => get();
+}
