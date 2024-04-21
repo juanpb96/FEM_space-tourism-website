24113a7a4510c66d01f62fcd98564b58f50a4bc1
Juan Bonilla <76854902+juanpb96@users.noreply.github.com>
2024-04-20 21:57:17 -0500

    feat(61): Add Prod Api Url (#63)

--- src/pages/Destination/adapter/getDestinationsAdapter.ts
+++ src/pages/Destination/adapter/getDestinationsAdapter.ts
@@ -1,14 +1,28 @@
 import type { Destinations } from "../types";
+import jsonData from '../../../services/data.json';
+
+// TODO: Move to a constants file once other endpoints are added
+const apiUrl = 'https://fem-space-tourism-backend.onrender.com/api';
+const endpoint = '/destinations';
 
 const getDestinations = async () => {
   try {
-    // TODO: Update URL when resource is defined for prod env
     // TODO: Test on mobile when prod gets defined
-    const response = await fetch('http://localhost:3000/destinations');
+    const response = await fetch(`${apiUrl}${endpoint}`);
+
+    if (!response.ok) {
+      throw new Error('Failed to fetch data from the backend');
+    }
+
     const data = await response.json();
     return data;
-  } catch (error) {
-    console.log(error);
+  } catch (error) {  
+    if (error instanceof Error) {
+      console.error(error.message);
+    }
+
+    // Returning local JSON data in case backend is not available for any reason
+    return jsonData.destinations;
   }
 }
 
