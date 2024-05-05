01f22e403ae1cd57f03cef43a9f844b3d85740b9
Juan Bonilla <76854902+juanpb96@users.noreply.github.com>
2024-05-05 12:59:09 -0500

    fix(66): Add basename in vite config (#68)

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig(({ 
+  base: '/FEM_space-tourism-website/',
   plugins: [react()],
   build: { assetsDir: '' }
 }));
