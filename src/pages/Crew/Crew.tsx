16e9039cb1e51e30aef00fa1985ae9fb3fc9934d
MasterDev <juanpb.96@gmail.com>
2024-05-18 19:58:00 -0500

    Add base structure of the page

--- src/pages/Crew/Crew.tsx
+++ src/pages/Crew/Crew.tsx
@@ -1,5 +1,29 @@
+import { CrewPagination } from "../../components/CrewPagination/CrewPagination";
+import { Description } from "../../components/Description/Description";
+import { Heading } from "../../components/Heading/Heading";
+import { Subtitle } from "../../components/Subtitle/Subtitle";
+
 export const Crew = () => {
   return (
-    <div>Crew</div>
+    <main>
+      <Subtitle
+        prefix="02"
+        title="Meet your crew"
+      />
+      <picture>
+        <source srcSet="./assets/crew/image-douglas-hurley.webp" type='image/webp' />
+        <img src="./assets/crew/image-douglas-hurley.png" alt="" />
+      </picture>
+      <hr/>
+      <CrewPagination />
+      <h4>Commander</h4>
+      <Heading
+        variant="small"
+        text="Douglas Hurley"
+      />
+      <Description>
+        Douglas Gerald Hurley is an American engineer, former Marine Corps pilot and former NASA astronaut. He launched into space for the third time as commander of Crew Dragon Demo-2.
+      </Description>
+    </main>
   );
 };
--- src/pages/Crew/stories/Crew.stories.tsx
+++ src/pages/Crew/stories/Crew.stories.tsx
@@ -1,13 +1,22 @@
 import type { Meta, StoryObj } from '@storybook/react';
 
 import { Crew } from '../Crew';
+import { mobileParameters } from '../../../components/constants/stories-viewports';
 
 const meta: Meta<typeof Crew> = {
   title: 'Pages/Crew',
   component: Crew,
+  parameters: {
+    layout: 'fullscreen'
+  }
 };
 
 export default meta;
 type Story = StoryObj<typeof Crew>;
 
+export const CrewOnMobile: Story = {
+  parameters: mobileParameters
+};
+
+export const CrewOnTablet: Story = {};
 export const Default: Story = {};
