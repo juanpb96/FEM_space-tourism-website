22a4daae8d130e556f9d63e60db4743e046d03a2
MasterDev <juanpb.96@gmail.com>
2024-05-27 17:22:21 -0500

    Add structure and styles for mobile view

--- src/pages/Crew/Crew.tsx
+++ src/pages/Crew/Crew.tsx
@@ -2,25 +2,27 @@ import { CrewPagination } from "../../components/CrewPagination/CrewPagination";
 import { Description } from "../../components/Description/Description";
 import { Heading } from "../../components/Heading/Heading";
 import { Subtitle } from "../../components/Subtitle/Subtitle";
+import styles from './styles/crew.module.scss';
 
 export const Crew = () => {
   return (
-    <main>
+    <main className={styles["wrapper"]}>
       <Subtitle
         prefix="02"
         title="Meet your crew"
       />
       <picture>
         <source srcSet="./assets/crew/image-douglas-hurley.webp" type='image/webp' />
-        <img src="./assets/crew/image-douglas-hurley.png" alt="" />
+        <img className={styles["crew-member"]} src="./assets/crew/image-douglas-hurley.png" alt="" />
       </picture>
-      <hr/>
+      <hr className={styles["divider"]}/>
       <CrewPagination />
-      <h4>Commander</h4>
+      <h4 className={styles["rank"]}>Commander</h4>
       <Heading
         variant="small"
         text="Douglas Hurley"
       />
+      <div className={styles["spacer"]}></div>
       <Description>
         Douglas Gerald Hurley is an American engineer, former Marine Corps pilot and former NASA astronaut. He launched into space for the third time as commander of Crew Dragon Demo-2.
       </Description>
