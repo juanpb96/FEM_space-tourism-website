ee6f551a798071ab9e8ddf8028ba809f7507c60f
MasterDev <juanpb.96@gmail.com>
2024-06-29 18:38:50 -0500

    Add Pagination component in Technology page

--- src/pages/Technology/Technology.tsx
+++ src/pages/Technology/Technology.tsx
@@ -1,6 +1,7 @@
 import { Description } from "../../components/Description/Description";
 import { Heading } from "../../components/Heading/Heading";
 import { Subtitle } from "../../components/Subtitle/Subtitle";
+import { Pagination } from "./components/Pagination";
 import styles from "./styles/technology.module.scss";
 
 export const Technology = () => {
@@ -10,19 +11,24 @@ export const Technology = () => {
         prefix="03"
         title="Space Launch 101"
       />
+
       <picture>
         <source srcSet="./assets/technology/image-launch-vehicle-landscape.avif" type='image/avif' />
         <img className={styles["photo"]} src="./assets/technology/image-launch-vehicle-landscape.jpg" alt="" />
       </picture>
+
       <div className={styles["pagination"]}>
-        Pagination 1 ... 2 ... 3
+        <Pagination />
       </div>
+
       <h4 className={styles["terminology"]}>THE TERMINOLOGY…</h4>
       <Heading
         variant="small"
         text="Launch Vehicle"
       />
+
       <div className={styles["spacer"]} />
+
       <Description>
         A launch vehicle or carrier rocket is a rocket-propelled vehicle used to carry a payload from Earth's surface to space, usually to Earth orbit or beyond. Our WEB-X carrier rocket is the most powerful in operation. Standing 150 metres tall, it's quite an awe-inspiring sight on the launch pad!
       </Description>
