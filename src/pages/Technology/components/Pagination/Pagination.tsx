973f67100c5fc272258e6e28ee4fc657d286f6d6
MasterDev <juanpb.96@gmail.com>
2024-06-29 18:10:35 -0500

    Add base styles for Pagination component

--- src/pages/Technology/components/Pagination/Pagination.tsx
+++ src/pages/Technology/components/Pagination/Pagination.tsx
@@ -3,7 +3,7 @@ import styles from "./styles/pagination.module.scss";
 export const Pagination = () => {
   return (
     <ol className={styles["wrapper"]}>
-      <li>1</li>
+      <li className={styles["active"]}>1</li>
       <li>2</li>
       <li>3</li>
     </ol>
