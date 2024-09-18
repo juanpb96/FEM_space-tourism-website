c3cacd50e9e5640703a583821cc6e1964ae25005
MasterDev <juanpb.96@gmail.com>
2024-09-17 19:10:41 -0500

    refactor(79): Adjust layout and styles

--- src/pages/Crew/Crew.tsx
+++ src/pages/Crew/Crew.tsx
@@ -2,15 +2,12 @@ import { CrewPagination } from "../../components/CrewPagination/CrewPagination";
 import { Description } from "../../components/Description/Description";
 import { Heading } from "../../components/Heading/Heading";
 import { Subtitle } from "../../components/Subtitle/Subtitle";
-import { useScreenType } from "../../hooks/useScreenType";
 import styles from "./styles/crew.module.scss";
 import { useCrew } from "./useCrew";
 
 // TODO: Check this article to get inspiration: https://www.freecodecamp.org/news/build-an-image-carousel-with-react-and-framer-motion/
 export const Crew = () => {
   const { crew, currentCrewMember, onPaginationClick } = useCrew();
-  const screenType = useScreenType();
-  const isSmallDevice = screenType === "mobile" || screenType === "tablet";
 
   // TODO: Consider adding a loading screen instead of returning nothing
   if (!currentCrewMember) {
@@ -21,9 +18,28 @@ export const Crew = () => {
     <main className={styles["wrapper"]}>
       <Subtitle prefix="02" title="Meet your crew" />
       <article>
+        <picture className={styles["photo-wrapper"]}>
+          <source srcSet={currentCrewMember.images.webp} type="image/webp" />
+          <img
+            className={styles["photo"]}
+            src={currentCrewMember.images.png}
+            alt=""
+          />
+        </picture>
+
+        <hr className={styles["divider"]} />
+
+        <div className={styles["pagination"]}>
+          <CrewPagination
+            crew={crew}
+            currentCrewMember={currentCrewMember}
+            onClick={onPaginationClick}
+          />
+        </div>
+
         <section className={styles["details"]}>
           <h4 className={styles["rank"]}>{currentCrewMember.role}</h4>
-          {/* TODO: Consider changing the heading tag based on a prop (i.e. as="h4") */}
+
           <Heading variant="small" text={currentCrewMember.name} />
 
           <div className={styles["spacer"]} />
@@ -31,35 +47,7 @@ export const Crew = () => {
           <div className={styles["description-wrapper"]}>
             <Description>{currentCrewMember.bio}</Description>
           </div>
-          {/* TODO: Need to pass custom styles and fix styles for all viewports */}
-          {!isSmallDevice && (
-            <CrewPagination
-              crew={crew}
-              currentCrewMember={currentCrewMember}
-              onClick={onPaginationClick}
-            />
-          )}
         </section>
-
-        {isSmallDevice && (
-          <CrewPagination
-            crew={crew}
-            currentCrewMember={currentCrewMember}
-            onClick={onPaginationClick}
-          />
-        )}
-
-        <div>
-          <picture>
-            <source srcSet={currentCrewMember.images.webp} type="image/webp" />
-            <img
-              className={styles["photo"]}
-              src={currentCrewMember.images.png}
-              alt=""
-            />
-          </picture>
-          <hr className={styles["divider"]} />
-        </div>
       </article>
     </main>
   );
