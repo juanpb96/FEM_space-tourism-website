083dec6e841bcaca809170614c23138956cea7bc
MasterDev <juanpb.96@gmail.com>
2025-05-04 18:24:39 -0500

    refactor(59): Update project breakpoints
    This commit changes the value for the tablet breakpoint from 768px to 600px and the desktop breakpoint from 1024px to 1300px.
    This change is made to ensure that the project is more responsive and better suited for a wider range of devices.

--- src/hooks/useScreenType.ts
+++ src/hooks/useScreenType.ts
@@ -1,37 +1,40 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState } from "react";
 
 const BREAKPOINTS = {
-  tablet: '(min-width: 48rem) and (max-width: 63.9375rem)',
-  desktop: '(min-width: 64rem) and (max-width: 89.9375rem)',
-  'large-desktop': '(min-width: 90rem)',
+  tablet: "(min-width: 37.5rem) and (max-width: 81.1875rem)",
+  desktop: "(min-width: 81.25rem) and (max-width: 89.9375rem)",
+  "large-desktop": "(min-width: 90rem)",
 };
 
-export type ScreenType = keyof typeof BREAKPOINTS | 'mobile';
+export type ScreenType = keyof typeof BREAKPOINTS | "mobile";
 
 const getWindowType = () => {
-  for (const breakpoint in BREAKPOINTS) {     
-    if (window.matchMedia(BREAKPOINTS[breakpoint as keyof typeof BREAKPOINTS]).matches) {
+  for (const breakpoint in BREAKPOINTS) {
+    if (
+      window.matchMedia(BREAKPOINTS[breakpoint as keyof typeof BREAKPOINTS])
+        .matches
+    ) {
       return breakpoint as keyof typeof BREAKPOINTS;
     }
   }
 
-  return 'mobile';
+  return "mobile";
 };
 
 export const useScreenType = () => {
-  const [screenType, setScreenType] = useState<ScreenType>(getWindowType); 
+  const [screenType, setScreenType] = useState<ScreenType>(getWindowType);
 
   useEffect(() => {
     const onResize = () => {
       setScreenType(getWindowType());
     };
 
-    window.addEventListener('resize', onResize);
-  
+    window.addEventListener("resize", onResize);
+
     return () => {
-      window.removeEventListener('resize', onResize);
-    }
-  }, []);  
+      window.removeEventListener("resize", onResize);
+    };
+  }, []);
 
   return screenType;
-}
+};
