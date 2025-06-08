44bd2a5bdb2215c82faf03f84ed33b130c650552
Juan Bonilla <76854902+juanpb96@users.noreply.github.com>
2025-06-08 15:03:04 -0500

    refactor(60): Add header-content wrapper (#107)

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,13 +1,15 @@
-import styles from './styles/header.module.scss';
-import { Logo } from '../Logo/Logo';
-import { NavigationBar } from '../NavigationBar/NavigationBar';
+import styles from "./styles/header.module.scss";
+import { Logo } from "../Logo/Logo";
+import { NavigationBar } from "../NavigationBar/NavigationBar";
 
 export const Header = () => {
   return (
-    <header className={styles['header']}>
-      <Logo />
-      <div className={styles['line']} />
-      <NavigationBar />
+    <header className={styles["header"]}>
+      <div className={styles["header-content"]}>
+        <Logo />
+        <div className={styles["line"]} />
+        <NavigationBar />
+      </div>
     </header>
   );
 };
