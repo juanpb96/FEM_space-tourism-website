7e266153103f31c65b1ccb4e22b741a8fc7f22a9
MasterDev <juanpb.96@gmail.com>
2025-07-02 13:30:01 -0500

    refactor(108): Include accessibility features in the MenuMobile component
    Implement the MenuButton component
    
    Add and remove focus when menu opens and closes
    
    Add a focus trap when the menu is open so the user can loop through the menu options
    
    Apply aria attributes for better interaction with the screen reader
    
    Add the display property in the menu transitions to prevent unnecessary focus when the menu is closed

--- src/components/NavigationBar/MenuMobile.tsx
+++ src/components/NavigationBar/MenuMobile.tsx
@@ -8,25 +8,53 @@ import {
 import { motion } from "framer-motion";
 import { menuVariants, optionsVariants } from "./animations/menu.variants";
 import styles from "./styles/menu.module.scss";
+import { MenuButton } from "./MenuButton";
 
 const PAGES = ["Home", "Destination", "Crew", "Technology"];
 
 interface MenuMobileProps {
   isOpen: boolean;
+  onToggle: () => void;
 }
 
-/**
- * TODO: Increase menu options clickable area to enhance usability - Issue #48
- * - Update hover query by including pointer fine
- */
+// TODO: Continue with the implementation following https://www.w3.org/WAI/ARIA/apg/patterns/dialog-modal/
 
 // TODO: Reduce dependency between MenuMobile and Menu
 
-export const MenuMobile = ({ isOpen }: MenuMobileProps) => {
+export const MenuMobile = ({ isOpen, onToggle }: MenuMobileProps) => {
   const [activeMenuOptionIndex, setActiveMenuOptionIndex] = useState(-1);
   const olRef = useRef<HTMLOListElement>(null);
   const barRef = useRef<HTMLDivElement>(null);
+  const buttonRef = useRef<HTMLButtonElement>(null);
   const location = useLocation();
+  const activeElement = document.activeElement as HTMLElement | null;
+  const interactiveElementsRef = useRef<
+    (HTMLAnchorElement | HTMLButtonElement | null)[]
+  >([]);
+
+  if (isOpen) {
+    document.body.style.overflow = "hidden";
+  }
+
+  if (!isOpen) {
+    activeElement?.focus();
+    document.body.style.overflow = "revert";
+  }
+
+  // Preferred way compared to onAnimationComplete since it ensures the focus is set during the animation
+  useEffect(() => {
+    let timeoutId: NodeJS.Timeout | undefined;
+
+    if (isOpen) {
+      timeoutId = setTimeout(() => {
+        interactiveElementsRef.current[0]?.focus();
+      }, 100);
+    }
+
+    return () => {
+      clearTimeout(timeoutId);
+    };
+  }, [isOpen]);
 
   useEffect(() => {
     const newIndex = PAGES.map((item) => item.toLowerCase()).indexOf(
@@ -38,23 +66,99 @@ export const MenuMobile = ({ isOpen }: MenuMobileProps) => {
     }
   }, [location.pathname, setActiveMenuOptionIndex]);
 
+  // TODO: Refactor this code to use a more declarative approach and commit changes
+  useEffect(() => {
+    let focusCloseButton = true;
+    let focusFirstLink = false;
+
+    if (!interactiveElementsRef.current.includes(buttonRef.current)) {
+      interactiveElementsRef.current.push(buttonRef.current);
+    }
+
+    const onKeyDown = (event: KeyboardEvent) => {
+      if (!isOpen) {
+        return;
+      }
+
+      if (document.activeElement === interactiveElementsRef.current[0]) {
+        focusCloseButton = true;
+      }
+
+      if (
+        document.activeElement ===
+        interactiveElementsRef.current[
+          interactiveElementsRef.current.length - 1
+        ]
+      ) {
+        focusFirstLink = true;
+      }
+
+      if (event.key === "Tab") {
+        if (event.shiftKey) {
+          focusFirstLink = false;
+          if (focusCloseButton) {
+            event.preventDefault();
+            buttonRef.current?.focus();
+            focusCloseButton = false;
+          } else if (
+            document.activeElement === interactiveElementsRef.current[1]
+          ) {
+            focusCloseButton = true;
+          }
+        } else {
+          focusCloseButton = false;
+          if (focusFirstLink) {
+            event.preventDefault();
+            interactiveElementsRef.current[0]?.focus();
+            focusFirstLink = false;
+          } else if (
+            document.activeElement ===
+            interactiveElementsRef.current[
+              interactiveElementsRef.current.length - 2
+            ]
+          ) {
+            focusFirstLink = true;
+          }
+        }
+      }
+    };
+
+    window.addEventListener("keydown", onKeyDown);
+
+    return () => {
+      window.removeEventListener("keydown", onKeyDown);
+    };
+  }, [isOpen]);
+
   const onLinkClick = (index: number) => {
     // TODO: Investigate if closing the Menu here enhances UX - Issue #48
     setActiveMenuOptionIndex(index);
   };
 
   return (
-    <motion.nav
-      initial={false}
-      animate={getMobileAnimation(isOpen)}
-      variants={optionsVariants}
+    <nav
       className={styles["menu"]}
+      role="navigation"
+      aria-label="Main Navigation"
     >
-      <ol ref={olRef}>
+      {isOpen && (
+        <div
+          className={styles.backdrop}
+          onClick={onToggle}
+          aria-hidden="true"
+        />
+      )}
+      <motion.ol
+        ref={olRef}
+        initial={false}
+        animate={getMobileAnimation(isOpen)}
+        variants={optionsVariants}
+      >
         {PAGES.map((page, index) => (
           <li key={page}>
             <NavLink
               to={`/${page}`}
+              ref={(el) => (interactiveElementsRef.current[index] = el)}
               className={getActiveClass}
               onClick={() => onLinkClick(index)}
             >
@@ -63,7 +167,8 @@ export const MenuMobile = ({ isOpen }: MenuMobileProps) => {
             </NavLink>
           </li>
         ))}
-      </ol>
+      </motion.ol>
+      {/* TODO: Fix bar as it is being shown always and overlapped when the menu opens - Issue #48 */}
       {activeMenuOptionIndex >= 0 && (
         <motion.div
           ref={barRef}
@@ -77,6 +182,7 @@ export const MenuMobile = ({ isOpen }: MenuMobileProps) => {
           variants={menuVariants}
         />
       )}
-    </motion.nav>
+      <MenuButton ref={buttonRef} isOpen={isOpen} onToggle={onToggle} />
+    </nav>
   );
 };
--- src/components/NavigationBar/animations/menu.variants.ts
+++ src/components/NavigationBar/animations/menu.variants.ts
@@ -1,61 +1,66 @@
-import { ScreenType } from '../../../hooks/useScreenType';
+import { ScreenType } from "../../../hooks/useScreenType";
 
 interface MenuVerticalVariantProps {
-  index: number,
+  index: number;
 }
 
 interface WithOptionsProps extends MenuVerticalVariantProps {
-  options: HTMLLIElement[],
+  options: HTMLLIElement[];
 }
 
 interface MenuHorizontalVariantProps extends WithOptionsProps {
-  screenType: ScreenType,
+  screenType: ScreenType;
 }
 
 // TODO: Check if it could be a better approach to move functions to another file
-const sumOptionsWidth = ({options, index}: WithOptionsProps) => {    
-  return options.slice(0, index).reduce((acc, option) => acc + option.offsetWidth, 0);
+const sumOptionsWidth = ({ options, index }: WithOptionsProps) => {
+  return options
+    .slice(0, index)
+    .reduce((acc, option) => acc + option.offsetWidth, 0);
 };
 
 // TODO: Create constant values for numbers without a clear explanation
-const calculateHorizontalMoveByViewport = ({screenType, options, index}: MenuHorizontalVariantProps) => {
-  const optionsGap = screenType === 'tablet' ? 38 : 48;
-  return sumOptionsWidth({options, index}) + index * optionsGap;
+const calculateHorizontalMoveByViewport = ({
+  screenType,
+  options,
+  index,
+}: MenuHorizontalVariantProps) => {
+  const optionsGap = screenType === "tablet" ? 38 : 48;
+  return sumOptionsWidth({ options, index }) + index * optionsGap;
 };
 
 const menuVariants = {
-  vertical: ({index}: MenuVerticalVariantProps) => ({
-    y: 118 + index * 51
+  vertical: ({ index }: MenuVerticalVariantProps) => ({
+    y: 118 + index * 51,
   }),
-  horizontal: ({screenType, options, index}: MenuHorizontalVariantProps) => ({
-    x: calculateHorizontalMoveByViewport({screenType, options, index})
+  horizontal: ({ screenType, options, index }: MenuHorizontalVariantProps) => ({
+    x: calculateHorizontalMoveByViewport({ screenType, options, index }),
   }),
-}
+};
 
 // TODO: Get more knowledge on the declaration added in the transition object
 const optionsVariants = {
-  open: { 
-    opacity: 1, 
-    x: 0, 
+  open: {
+    opacity: 1,
+    x: 0,
+    display: "flex",
     transition: {
       type: "spring",
       stiffness: 50,
-      restDelta: 2
-    }
+      restDelta: 2,
+    },
   },
-  closed: { 
-    opacity: 0, 
+  closed: {
+    opacity: 0,
     x: "100%",
+    display: "none",
     transition: {
       delay: 0.1,
       type: "spring",
       stiffness: 400,
-      damping: 40
-    }
+      damping: 40,
+    },
   },
 };
 
-export {
-  menuVariants,
-  optionsVariants
-};
+export { menuVariants, optionsVariants };
