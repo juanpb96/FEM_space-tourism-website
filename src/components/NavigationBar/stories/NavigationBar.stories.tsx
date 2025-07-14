d909d97cd52b9109fe9660b916ed60c58622bf94
MasterDev <juanpb.96@gmail.com>
2025-07-14 18:22:53 -0500

    fix(108): Update interaction test for the Navigation Bar on mobile
    Menu should be closed at first and after a link has been clicked

--- src/components/NavigationBar/stories/NavigationBar.stories.tsx
+++ src/components/NavigationBar/stories/NavigationBar.stories.tsx
@@ -1,26 +1,29 @@
-import type { Decorator, Meta, StoryObj } from '@storybook/react';
+import type { Decorator, Meta, StoryObj } from "@storybook/react";
 
-import { NavigationBar } from '../NavigationBar';
-import { defaultViewport, chromaticViewport } from '../../constants/stories-viewports';
-import { userEvent, within } from '@storybook/testing-library';
-import { expect } from '@storybook/jest';
-import { withRouter } from '../../helpers/stories/withRouter';
+import { NavigationBar } from "../NavigationBar";
+import {
+  defaultViewport,
+  chromaticViewport,
+} from "../../constants/stories-viewports";
+import { userEvent, within } from "@storybook/testing-library";
+import { expect } from "@storybook/jest";
+import { withRouter } from "../../helpers/stories/withRouter";
 
 const meta = {
-  title: 'Components/NavigationBar/NavigationBar',
+  title: "Components/NavigationBar/NavigationBar",
   component: NavigationBar,
-  tags: ['autodocs'],
+  tags: ["autodocs"],
   decorators: [withRouter],
   parameters: {
-    layout: 'fullscreen'
+    layout: "fullscreen",
   },
   play: async ({ canvasElement }) => {
     const canvas = within(canvasElement);
-    const anchorElement = canvas.getByRole('link', { name: /technology/i });
+    const anchorElement = canvas.getByRole("link", { name: /technology/i });
 
     await expect(anchorElement).toBeInTheDocument();
     await userEvent.click(anchorElement);
-  }
+  },
 } satisfies Meta<typeof NavigationBar>;
 
 export default meta;
@@ -29,33 +32,36 @@ type Story = StoryObj<typeof meta>;
 export const NavigationBarOnMobile: Story = {
   parameters: {
     viewport: {
-      defaultViewport: defaultViewport.mobile
+      defaultViewport: defaultViewport.mobile,
     },
     chromatic: {
-      viewports: [chromaticViewport.mobile]
+      viewports: [chromaticViewport.mobile],
     },
   },
   decorators: [
     (Story) => (
-      <div style={{ padding: '24px' }}>
+      <div style={{ padding: "24px" }}>
         <Story />
       </div>
     ),
   ],
   play: async ({ canvasElement }) => {
     const canvas = within(canvasElement);
-    const button = await canvas.findByRole('button', { name: /menu/i });
-    const anchorElement = canvas.getByRole('link', { name: /technology/i });
-
-    await expect(button).toBeInTheDocument();
+    const button = canvas.getByRole("button", { name: /open menu/i });
     await userEvent.click(button);
-    await expect(anchorElement).toBeInTheDocument();
+
+    const anchorElement = canvas.getByRole("link", {
+      name: /technology/i,
+    });
+
+    await expect(button.ariaExpanded).toBe("true");
     await userEvent.click(anchorElement);
-  }
+    await expect(button.ariaExpanded).toBe("false");
+  },
 };
 
 const withWrapper: Decorator = (Story) => (
-  <div style={{ display: 'flex', justifyContent: 'flex-end' }}>
+  <div style={{ display: "flex", justifyContent: "flex-end" }}>
     <Story />
   </div>
 );
@@ -63,15 +69,15 @@ const withWrapper: Decorator = (Story) => (
 export const NavigationBarOnTablet: Story = {
   parameters: {
     viewport: {
-      defaultViewport: defaultViewport.tablet
+      defaultViewport: defaultViewport.tablet,
     },
     chromatic: {
-      viewports: [chromaticViewport.tablet]
-    }
+      viewports: [chromaticViewport.tablet],
+    },
   },
   decorators: [withWrapper],
 };
 
 export const NavigationBarDesktop: Story = {
   decorators: [withWrapper],
-}
+};
